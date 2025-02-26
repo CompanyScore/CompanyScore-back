beaa0fb924a6758c835bb813efc923e5c1a40a37
Dimash <dinmukhamed.amirov@gmail.com>
2025-02-26 16:54:54 +0500

    feat: add user swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -5,47 +5,79 @@ import {
   IsBoolean,
   IsDate,
 } from 'class-validator';
-
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 export class CreateUserDto {
   @IsString()
   @IsOptional()
+  @ApiPropertyOptional({
+    example: 'some-refresh-token',
+    description: 'Refresh token for the user (optional)',
+  })
   refreshToken?: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'user', description: 'The role of the user' })
   role?: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'linkedin-12345',
+    description: 'LinkedIn ID of the user',
+  })
   linkedinId: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'John Doe', description: 'The name of the user' })
   name?: string;
 
   @IsOptional()
   @IsNotEmpty()
+  @ApiPropertyOptional({
+    example: 'file/users/avatars/avatar.png',
+    description: 'Avatar URL for the user (optional)',
+  })
   avatar?: string;
-
-  // @IsNotEmpty() // Файлы не должны быть частью DTO, потому что class-validator не может их валидировать.
-  // avatarFile?: Express.Multer.File;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'Software Engineer', description: 'User position' })
   position?: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'A brief description about the user',
+    description: 'User description',
+  })
   description?: string;
 
   @IsNotEmpty()
   @IsDate()
+  @ApiProperty({
+    example: '2023-01-01T00:00:00.000Z',
+    description: 'Creation date of the user',
+    type: String,
+    format: 'date-time',
+  })
   createDate?: Date;
 
   @IsNotEmpty()
   @IsDate()
+  @ApiProperty({
+    example: '2023-12-31T23:59:59.999Z',
+    description: 'Deletion date of the user',
+    type: String,
+    format: 'date-time',
+  })
   deleteDate?: Date;
 
   @IsNotEmpty()
   @IsBoolean()
+  @ApiProperty({
+    example: false,
+    description: 'Flag indicating whether the user is deleted',
+  })
   isDeleted?: boolean;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -21,31 +21,54 @@ import { Roles } from 'src/decorators/roles.decorator';
 import { ImageFormatInterceptor } from 'src/interceptors/image-format.interceptor';
 
 import * as multer from 'multer';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { USER_RESPONSE, USERS_RESPONSE } from './user.swagger.responses';
 
+@ApiTags('users') // This tag will appear in the Swagger UI
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Users list',
+    ...USERS_RESPONSE,
+  })
   async findAll(@Query('isDeleted') isDeleted: boolean) {
     return this.usersService.findAll(isDeleted);
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'User detail',
+    ...USER_RESPONSE,
+
+  })
+  @ApiResponse({ status: 404, description: 'User not found.' })
   async findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Updated user',
+    ...USER_RESPONSE,
+  })
   @UseInterceptors(
-    FileInterceptor('avatarFile', {
-      storage: multer.memoryStorage(),
-    }),
+    FileInterceptor('avatarFile', { storage: multer.memoryStorage() }),
     ImageFormatInterceptor,
   )
   async update(
@@ -58,6 +81,12 @@ export class UsersController {
 
   @Roles(Role.ADMIN)
   @Delete(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Deleted user',
+    ...USER_RESPONSE,
+
+  })
   async remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
