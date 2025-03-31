63af30928af52832659098dc90ad2da5fdd6de58
Dimash <dinmukhamed.amirov@gmail.com>
2025-03-31 15:54:11 +0500

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,21 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Обрабатываем preflight-запросы (OPTIONS)
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', process.env.FRONT_URL);
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.status(200).send();
+    } else {
+      next();
+    }
+  });
+
   app.enableCors({
     origin: process.env.FRONT_URL,
     // Указываем фронтенд
