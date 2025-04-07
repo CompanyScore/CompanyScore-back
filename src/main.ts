ac7975f832bd510a44125747f7788ea4cd60b851
Dimash <dinmukhamed.amirov@gmail.com>
2025-04-07 18:12:09 +0500

    fix: add options

--- src/main.ts
+++ src/main.ts
@@ -9,35 +9,35 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Обрабатываем preflight-запросы (OPTIONS) https://companyscore.net/
-  // app.use((req, res, next) => {
-  //   console.log('Origin:', req.headers.origin); // Добавьте это для проверки
+  app.use((req, res, next) => {
+    console.log('Origin:', req.headers.origin); // Добавьте это для проверки
 
-  //   const allowedOrigins = [
-  //     'https://companyscore.net',
-  //     'https://api.companyscore.net',
-  //   ];
-  //   const origin = req.headers.origin;
+    const allowedOrigins = [
+      'https://companyscore.net',
+      'https://api.companyscore.net',
+    ];
+    const origin = req.headers.origin;
 
-  //   if (allowedOrigins.includes(origin)) {
-  //     res.header('Access-Control-Allow-Origin', origin);
-  //   }
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
 
-  //   res.header(
-  //     'Access-Control-Allow-Methods',
-  //     'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-  //   );
-  //   res.header(
-  //     'Access-Control-Allow-Headers',
-  //     'Content-Type, Authorization, Cache-Control, Pragma, Expires',
-  //   );
-  //   res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, Cache-Control, Pragma, Expires',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
 
-  //   if (req.method === 'OPTIONS') {
-  //     res.status(200).send();
-  //   } else {
-  //     next();
-  //   }
-  // });
+    if (req.method === 'OPTIONS') {
+      res.status(200).send();
+    } else {
+      next();
+    }
+  });
 
   // app.use(
   //   helmet({
