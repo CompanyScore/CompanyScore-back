f4a5ff7127e22da8b5b6551ae3cd319b0c00f41d
Adil <kemelbekov-adil@mail.ru>
2025-04-13 11:57:08 +0500

    chore: update main

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
-// import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Обрабатываем preflight-запросы (OPTIONS) https://companyscore.net/
-  app.use((req, res, next) => {
-    console.log('Origin:', req.headers.origin); // Добавьте это для проверки
-
-    const allowedOrigins = [
-      'https://companyscore.net',
-      'https://api.companyscore.net',
-    ];
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization, Cache-Control, Pragma, Expires',
-    );
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      res.status(200).send();
-    } else {
-      next();
-    }
-  });
-
-  // app.use(
-  //   helmet({
-  //     crossOriginResourcePolicy: { policy: 'cross-origin' },
-  //   }),
-  // );
-
-  // app.enableCors({
-  //   origin: 'https://companyscore.net',
-  //   credentials: true,
-  // });
-
   app.enableCors({
     origin: ['https://companyscore.net', 'https://api.companyscore.net'],
+    credentials: true,
+    methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
     allowedHeaders: [
       'Content-Type',
       'Authorization',
@@ -59,22 +16,11 @@ async function bootstrap() {
       'Pragma',
       'Expires',
     ],
-    credentials: true, // Разрешаем отправку куки
-    methods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
   });
 
   app.use(cookieParser());
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('CompanyScore')
-  //   .setDescription('The CompanyScore API description')
-  //   .setVersion('1.0')
-  //   .addTag('CompanyScore')
-  //   .build();
-  // const documentFactory = () => SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, documentFactory);
-
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8000;
   await app.listen(port);
 }
 bootstrap();
