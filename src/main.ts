5ade83dcab6576b7a3c34ee23143587aef26e11d
Adil <kemelbekov-adil@mail.ru>
2025-05-18 01:23:11 +0500

    chore: return linkedin

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
-import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        secure: process.env.SESSION_COOKIE_SECURE === 'true',
-        maxAge:
-          Number(process.env.SESSION_COOKIE_MAXAGE) || 24 * 60 * 60 * 1000,
-      },
-    }),
-  );
 
   app.enableCors({
     origin: [
