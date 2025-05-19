8b3cd93fcfa55a64bd42a1ab7c27ecb2a8e4a206
Dimash <dinmukhamed.amirov@gmail.com>
2025-05-19 20:52:35 +0500

    eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,25 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist', 'node_modules'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+];
