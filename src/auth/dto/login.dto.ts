822ae45b3d4baaac319f87d9c6797614c966fcd9
Timur <timurlin212@gmail.com>
2025-07-01 17:13:42 +0300

    Added messages about failed validations for registration and login.

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,13 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsEmail()
+  @IsNotEmpty({ message: 'Email не может быть пустым' })
+  @IsEmail({}, { message: 'Неверный формат email' })
   email: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Пароль не может быть пустым' })
+  @Type(() => String)
+  @MinLength(8, { message: 'Пароль должен содержать минимум 8 символов' })
   password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,13 +1,17 @@
+import { Type } from 'class-transformer';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDto {
-  @IsEmail()
+  @IsNotEmpty({ message: 'Email не может быть пустым' })
+  @IsEmail({}, { message: 'Неверный формат email' })
   email: string;
 
-  @IsNotEmpty()
-  @MinLength(8)
+  @IsNotEmpty({ message: 'Пароль не может быть пустым' })
+  @Type(() => String)
+  @MinLength(8, { message: 'Пароль должен содержать минимум 8 символов' })
   password: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Имя не может быть пустым' })
+  @Type(() => String)
   name: string;
 }
