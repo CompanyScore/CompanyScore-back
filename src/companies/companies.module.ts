99cd17be2c83cdce54e695df5eae0168aa41b281
Dimash <dinmukhamed.amirov@gmail.com>
2025-07-01 10:30:13 +0500

    fix: change spaces to r2

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './entities/company.entity';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
-import { SpacesService } from 'src/providers/space.service';
+import { R2Service } from 'src/providers/r2.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Company])],
   controllers: [CompaniesController],
-  providers: [CompaniesService, SpacesService],
+  providers: [CompaniesService, R2Service],
   exports: [TypeOrmModule],
 })
 export class CompaniesModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { SpacesService } from 'src/providers/space.service';
+import { R2Service } from 'src/providers/r2.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, SpacesService],
-  exports: [TypeOrmModule, SpacesService, UsersService],
+  providers: [UsersService, R2Service],
+  exports: [TypeOrmModule, R2Service, UsersService],
 })
 export class UsersModule {}
